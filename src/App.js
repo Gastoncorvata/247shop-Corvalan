86370cfa92f1632e2400177cf53d05e89396c14c
Gaston Corvalan <corvata@gmail.com>
2021-03-10 12:04:03 -0300

    categorias navbar + cart + itemList

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import NavBar from './components/NavBar.js'
+import NavBar from './components/NavBar/NavBar.jsx'
+import ItemListContainer from "./components/ItemListContainer"
 import './index.css';
 import './App.css';
 
@@ -6,7 +7,8 @@ function App() {
 	return (
 		<div>
 			<NavBar/>
-			<p className="text-2xl font-bold leading-relaxed mt-0 mb-4 text-indigo-500 content-center flex justify-center ">Este es el desafio de NavBar Component</p><p className="flex justify-center text-base font-ligth leading-relaxed mt-0 mb-4 text-blue-500 content-center animate-ping">By Gaston Corvalan</p>
+			<ItemListContainer/>
+			
 		</div>
 	);
 }
---
+++ src/components/CartWidget/index.jsx
@@ -0,0 +1,6 @@
+  
+import React from "react";
+
+export default function CartWidget() {
+  return <i class="fas fa-shopping-cart">  Carrito</i>;
+}
